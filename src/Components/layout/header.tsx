11f26aeb9a6f4b6136696e0c5d3c1871e9303d17
Éric  António <ericantoniodev@gmail.com>
2024-08-06 22:00:14 +0200

    Update header.tsx

--- src/Components/layout/header.tsx
+++ src/Components/layout/header.tsx
@@ -1,10 +1,23 @@
+import { Layout, Space } from "antd";
+import LayoutProvider from ".";
 import CurrentUser from "./current-user";
 
 const HeaderComponent = () => {
+  const headerStyles : React.CSSProperties ={
+    background: '#FFF',
+    display:'flex',
+    justifyContent:'flex-end',
+    padding:'0 24px',
+    position:'sticky',
+    top:'0',
+    zIndex:'999'
+  }
   return (
-    <div>
-      <CurrentUser/>
-    </div>
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size={"middle"}>
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
   );
 };
 
