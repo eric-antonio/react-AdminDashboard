3cb37de2a06ac383b87e0c28a41ea36ad0e2e8b7
Éric  António <ericantoniodev@gmail.com>
2024-08-06 16:10:01 +0200

    Update current-user.tsx

--- src/Components/layout/current-user.tsx
+++ src/Components/layout/current-user.tsx
@@ -1,7 +1,12 @@
 import { Popover, Button } from "antd";
 import React from "react";
+import CustomAvatar from "../custom-avatar";
+import { useGetIdentity } from "@refinedev/core";
+
+import type { User } from "@/graphql/schema.types";
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<Usere>();
   return (
     <>
       <Popover
@@ -10,7 +15,7 @@ const CurrentUser = () => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
       >
-        Test
+        <CustomAvatar />
       </Popover>
     </>
   );
