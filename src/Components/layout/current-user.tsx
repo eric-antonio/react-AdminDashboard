27ea7b94941eb4e1ad4945349d793c0d26b80b8f
Éric  António <ericantoniodev@gmail.com>
2024-08-06 17:30:37 +0200

    .

--- src/Components/layout/current-user.tsx
+++ src/Components/layout/current-user.tsx
@@ -2,11 +2,35 @@ import { Popover, Button } from "antd";
 import React from "react";
 import CustomAvatar from "../custom-avatar";
 import { useGetIdentity } from "@refinedev/core";
+import { Text } from "../text";
+import { useState } from "react";
 
 import type { User } from "@/graphql/schema.types";
+import { SettingOutlined } from "@ant-design/icons";
 
 const CurrentUser = () => {
-  const { data: user } = useGetIdentity<Usere>();
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: user } = useGetIdentity<User>();
+
+  const content = (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Text strong style={{ padding: "12px 20px" }}>
+        {user?.name}
+      </Text>
+      <div >
+        <Button
+        style={{textAlign:'left'}}
+        icon={<SettingOutlined />}
+        type="text"
+        block
+        onClick={()=> setIsOpen(true)}
+        >
+          Account Settings
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Popover
@@ -15,7 +39,12 @@ const CurrentUser = () => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
       >
-        <CustomAvatar />
+        <CustomAvatar
+          name={user?.name || ""}
+          src={user?.avatarUrl || ""}
+          size={32}
+          style={{ cursor: "pointer" }}
+        />
       </Popover>
     </>
   );
