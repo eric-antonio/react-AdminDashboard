684b2cf7f5264890040216296a19091a432617fe
Éric  António <ericantoniodev@gmail.com>
2024-08-06 23:18:06 +0200

    skeltons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { DealsChart, UpcomingEvents } from "@/Components/home";
+import { Col, Row } from "antd";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Row gutter={[32, 32]} style={{
+        marginTop:'32px'
+      }}>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          <UpcomingEvents/>
+        </Col>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          <DealsChart/>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
