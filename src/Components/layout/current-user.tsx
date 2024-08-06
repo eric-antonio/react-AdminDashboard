3e1631cf7eab4445e9e893aea5e180f0e07f9db4
Éric  António <ericantoniodev@gmail.com>
2024-08-06 18:12:22 +0200

    AccountSettings

--- src/Components/layout/current-user.tsx
+++ src/Components/layout/current-user.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 import type { User } from "@/graphql/schema.types";
 import { SettingOutlined } from "@ant-design/icons";
+import { AccountSettings } from "./account-settings";
 
 const CurrentUser = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,13 +18,18 @@ const CurrentUser = () => {
       <Text strong style={{ padding: "12px 20px" }}>
         {user?.name}
       </Text>
-      <div >
+      <div style={{ borderTop: "1px solid #d9d9d9", 
+        padding:'4px',
+        display:'flex',
+        flexDirection:'column',
+        gap:'4px'
+      }}>
         <Button
-        style={{textAlign:'left'}}
-        icon={<SettingOutlined />}
-        type="text"
-        block
-        onClick={()=> setIsOpen(true)}
+          style={{ textAlign: "left" }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setIsOpen(true)}
         >
           Account Settings
         </Button>
@@ -38,6 +44,7 @@ const CurrentUser = () => {
         trigger="click"
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
+        content={content}
       >
         <CustomAvatar
           name={user?.name || ""}
@@ -46,6 +53,11 @@ const CurrentUser = () => {
           style={{ cursor: "pointer" }}
         />
       </Popover>
+      {user && <AccountSettings
+        opened={isOpen}
+        setOpened={setIsOpen}
+        userId={user.id}
+      />}
     </>
   );
 };
