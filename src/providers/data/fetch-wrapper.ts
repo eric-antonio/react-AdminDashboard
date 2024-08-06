ea341ec56574fb02be354539cc8d6e826ca10801
Éric  António <ericantoniodev@gmail.com>
2024-08-06 11:50:15 +0200

    Update fetch-wrapper.ts

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,12 +1,11 @@
-import  {GraphQLFormattedError} from "graphql";
+import { GraphQLFormattedError } from "graphql";
 
 type Error = {
-    message: String;
-    statusCode: string;
-}
+  message: String;
+  statusCode: string;
+};
 
-
-const customFecth = async (url: string, options: RequestInit) => {
+const customFetch = async (url: string, options: RequestInit) => {
   const accsessTonken = localStorage.getItem("access_token");
 
   const headers = options.headers as Record<string, string>;
@@ -23,24 +22,35 @@ const customFecth = async (url: string, options: RequestInit) => {
 const getGraphQLErrors = (
   body: Record<"errors", GraphQLFormattedError[] | undefined>
 ): Error | null => {
+  if (!body) {
+    return {
+      message: "Unknown error",
+      statusCode: " INTERNAL_SERVER_ERROR",
+    };
+  }
 
-    if(!body){
-        return{
-            message: 'Unknown error',
-            statusCode: " INTERNAL_SERVER_ERROR"
-        }
-    }
+  if ("error" in body) {
+    const errors = body?.errors;
 
-    if("error" in body){
-        const errors = body?.errors;
-        
-        const messages = errors?.map((errors)=>errors?.message)?.join("");
-        const code = errors?.[0]?.extensions?.code;
+    const messages = errors?.map((errors) => errors?.message)?.join("");
+    const code = errors?.[0]?.extensions?.code;
 
-        return{
-            message: messages || JSON.stringify(errors);
-            statusCode: code || 500
-        }
-    }
-    return null; 
+    return {
+      message: messages || JSON.stringify(errors),
+      statusCode: code || 500,
+    };
+  }
+  return null;
+};
+
+const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
+  const responseClone = response.clone();
+  const body = await responseClone.json();
+
+  const error = getGraphQLErrors(body);
+  if (error) {
+    throw error;
+  }
+  return response;
 };
