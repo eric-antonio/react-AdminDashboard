98784b4a1923bfb275913cc8ab697e530e4f0e77
Éric  António <ericantoniodev@gmail.com>
2024-08-07 00:31:58 +0200

    Update deals-chart.tsx

--- src/Components/home/deals-chart.tsx
+++ src/Components/home/deals-chart.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import React from "react";
+import { Text } from "../text";
+import { config } from "process";
+import { Area, AreaConfig } from "@ant-design/plots";
 
 const DealsChart = () => {
-  return (
-    <div>DealsChart</div>
-  )
-}
 
-export default DealsChart
+  const config: AreaConfig ={
+    data:[]
+  }
+  return (
+    <Card
+    headStyle={{padding:'8px 16px'}}
+    bodyStyle={{padding:'24px 24px 0 24px'}}
+      style={{
+        height: "100%",
+        alignItems:'center',
+        gap:"8px"
+      }}
+      title={
+        <div>
+          <DollarOutlined/>
+          <Text size="sm"style={{marginLeft:'0.5rem'}}>Deals Chart</Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
+};
+
+export default DealsChart;
