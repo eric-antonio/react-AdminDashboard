68d7603390e97a5ad5e44cee8f85d7b44f7353f3
Éric  António <ericantoniodev@gmail.com>
2024-08-06 11:45:10 +0200

    Update index.tsx

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,6 +1,7 @@
 import { GraphQLClient } from "@refinedev/nestjs-query";
-import { error } from "console";
-export const API_URL: "http://api.crm.refine.dev";
+
+
+export const API_URL: "https://api.crm.refine.dev";
 
 export const client = new GraphQLClient(API_URL,{
     fetch:(url:string,options:RequestInit)=>{
