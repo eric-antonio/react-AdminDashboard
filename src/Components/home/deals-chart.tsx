d2afbaed77e3ff58d4c5adb14eaad2f83228f93a
Éric  António <ericantoniodev@gmail.com>
2024-08-07 11:49:23 +0200

    DealsChart

--- src/Components/home/deals-chart.tsx
+++ src/Components/home/deals-chart.tsx
@@ -4,26 +4,75 @@ import React from "react";
 import { Text } from "../text";
 import { config } from "process";
 import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
 
-  // const {data } = uselistDealsQuery({})
-  const config: AreaConfig ={
-    data:[]
-  }
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        };
+      },
+    },
+  };
   return (
     <Card
-    headStyle={{padding:'8px 16px'}}
-    bodyStyle={{padding:'24px 24px 0 24px'}}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "24px 24px 0 24px" }}
       style={{
         height: "100%",
-        alignItems:'center',
-        gap:"8px"
+        alignItems: "center",
+        gap: "8px",
       }}
       title={
         <div>
-          <DollarOutlined/>
-          <Text size="sm"style={{marginLeft:'0.5rem'}}>Deals Chart</Text>
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Deals Chart
+          </Text>
         </div>
       }
     >
