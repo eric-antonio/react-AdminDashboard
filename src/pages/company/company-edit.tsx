8aeca0e62fb17fae872c13828898dff2d6001652
Éric  António <ericantoniodev@gmail.com>
2024-08-08 14:12:29 +0200

    Update company-edit.tsx

--- src/pages/company/company-edit.tsx
+++ src/pages/company/company-edit.tsx
@@ -6,9 +6,14 @@ import { Edit } from "@refinedev/antd";
 import { useForm } from "@refinedev/core";
 import { useSelect } from "@refinedev/antd";
 import { GetFieldsFromList } from "@refinedev/nestjs-query";
-import { Row, Col, Form, Select } from "antd";
+import { Row, Col, Form, Select, Input, InputNumber } from "antd";
 import { USERS_SELECT_QUERY } from "@/graphql/queries";
 import SelectOptionWithAvatar from "@/Components/select-option-withAvatar";
+import {
+  businessTypeOptions,
+  companySizeOptions,
+  industryOptions,
+} from "@/constants";
 
 const EditCompany = () => {
   const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
@@ -23,7 +28,9 @@ const EditCompany = () => {
     GetFieldsFromList<UsersSelectQuery>
   >({
     resource: "users",
-    optionLabel: "name",
+    pagination: {
+      mode: "off",
+    },
     meta: {
       gqlQuery: USERS_SELECT_QUERY,
     },
@@ -43,28 +50,54 @@ const EditCompany = () => {
                 shape="square"
                 src={avatarUrl}
                 name={getNameInitials(name || "")}
-                style={{ marginBottom: '24px', width: 96, height: 96 }}
+                style={{ marginBottom: "24px", width: 96, height: 96 }}
               />
-               <Form.Item
-            label="Sales Owner"
-            name="salesOwnerId" // Corrected field name
-            rules={[{ required: true }]}
-          >
-            <Select
-              placeholder="Please select a sales owner"
-              {...selectProps}
-              options={queryResultUsers.data?.data.map((user) => ({
-                value: user.id, // Ensure this matches the expected ID type
-                label: (
-                  <SelectOptionWithAvatar
-                    name={user.name}
-                    avatarUrl={user.avatarUrl ?? undefined}
-                    shape={"circle"}
-                  />
-                ),
-              })) ?? []}
-            />
-          </Form.Item>
+              <Form.Item
+                label="Sales Owner"
+                name="salesOwnerId" // Corrected field name
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Please select a sales owner"
+                  {...selectProps}
+                  options={
+                    queryResultUsers.data?.data.map((user) => ({
+                      value: user.id, // Ensure this matches the expected ID type
+                      label: (
+                        <SelectOptionWithAvatar
+                          name={user.name}
+                          avatarUrl={user.avatarUrl ?? undefined}
+                          shape={"circle"}
+                        />
+                      ),
+                    })) ?? []
+                  }
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Select options={companySizeOptions} />
+              </Form.Item>
+
+              <Form.Item>
+                <InputNumber autoFocus addonBefore="MZN" placeholder="0,00" />
+              </Form.Item>
+
+              <Form.Item label="Industry">
+                <Select options={industryOptions} />
+              </Form.Item>
+
+              <Form.Item label="Business Type">
+                <Select options={businessTypeOptions} />
+              </Form.Item>
+
+              <Form.Item label="Country" name="country">
+                <Input placeholder="Country" />
+              </Form.Item>
+
+              <Form.Item label="WebSite" name="website">
+                <Input placeholder="Website" />
+              </Form.Item>
             </Form>
           </Edit>
         </Col>
