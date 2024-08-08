362a49956240bed2617b8d1f9cf5f3f412327753
Éric  António <ericantoniodev@gmail.com>
2024-08-08 13:49:35 +0200

    Update company-edit.tsx

--- src/pages/company/company-edit.tsx
+++ src/pages/company/company-edit.tsx
@@ -1,9 +1,14 @@
 import CustomAvatar from "@/Components/custom-avatar";
 import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { UsersSelectQuery } from "@/graphql/types";
 import { getNameInitials } from "@/utilities";
 import { Edit } from "@refinedev/antd";
 import { useForm } from "@refinedev/core";
-import { Row, Col, Form } from "antd";
+import { useSelect } from "@refinedev/antd";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { Row, Col, Form, Select } from "antd";
+import { USERS_SELECT_QUERY } from "@/graphql/queries";
+import SelectOptionWithAvatar from "@/Components/select-option-withAvatar";
 
 const EditCompany = () => {
   const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
@@ -13,6 +18,17 @@ const EditCompany = () => {
     },
   });
   const { avatarUrl, name } = queryResult?.data?.data || {};
+
+  const { selectProps, queryResult: queryResultUsers } = useSelect<
+    GetFieldsFromList<UsersSelectQuery>
+  >({
+    resource: "users",
+    optionLabel: "name",
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
+    },
+  });
+
   return (
     <div>
       <Row gutter={[32, 32]}>
@@ -27,8 +43,28 @@ const EditCompany = () => {
                 shape="square"
                 src={avatarUrl}
                 name={getNameInitials(name || "")}
-                style={{ marginBottom: 24, width: 96, height: 96 }}
+                style={{ marginBottom: '24px', width: 96, height: 96 }}
               />
+               <Form.Item
+            label="Sales Owner"
+            name="salesOwnerId" // Corrected field name
+            rules={[{ required: true }]}
+          >
+            <Select
+              placeholder="Please select a sales owner"
+              {...selectProps}
+              options={queryResultUsers.data?.data.map((user) => ({
+                value: user.id, // Ensure this matches the expected ID type
+                label: (
+                  <SelectOptionWithAvatar
+                    name={user.name}
+                    avatarUrl={user.avatarUrl ?? undefined}
+                    shape={"circle"}
+                  />
+                ),
+              })) ?? []}
+            />
+          </Form.Item>
             </Form>
           </Edit>
         </Col>
