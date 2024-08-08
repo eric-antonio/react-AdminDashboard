e9fb827e57f7144037e96304a7f52ed153ac58f5
Éric  António <ericantoniodev@gmail.com>
2024-08-08 12:36:40 +0200

    Create select-option-withAvatar.tsx

---
+++ src/Components/select-option-withAvatar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+type Props = {
+  avatarUrl?: string,
+  name: string,
+  shape: 'circle' | 'square'
+}
+
+const SelectOptionWithAvatar = ({avatarUrl, name , shape}:Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SelectOptionWithAvatar
