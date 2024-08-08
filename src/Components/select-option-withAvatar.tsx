87e8a146885ddd39597d77addb2aedc674df90fa
Éric  António <ericantoniodev@gmail.com>
2024-08-08 12:40:48 +0200

    Update select-option-withAvatar.tsx

--- src/Components/select-option-withAvatar.tsx
+++ src/Components/select-option-withAvatar.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+import CustomAvatar from "./custom-avatar";
+import { Text } from "./text";
 type Props = {
-  avatarUrl?: string,
-  name: string,
-  shape: 'circle' | 'square'
-}
+  avatarUrl?: string;
+  name: string;
+  shape: "circle" | "square";
+};
 
-const SelectOptionWithAvatar = ({avatarUrl, name , shape}:Props) => {
+const SelectOptionWithAvatar = ({ avatarUrl, name, shape }: Props) => {
   return (
-    <div>
-      
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "8",
+      }}
+    >
+      <CustomAvatar shape={shape} src={avatarUrl} size={24} />
+      <Text style={{ marginLeft: "10px" }}>{name}</Text>
     </div>
-  )
-}
+  );
+};
 
-export default SelectOptionWithAvatar
+export default SelectOptionWithAvatar;
