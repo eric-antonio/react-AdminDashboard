10b1349d2d9168f50c714e351ef3f2b51d850c78
Éric  António <ericantoniodev@gmail.com>
2024-08-08 17:24:48 +0200

    Update list.tsx

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {
+  KanbanBoard,
+  KanbanBoardContainer,
+} from "@/Components/tasks/kanban/board";
+import { KanbanColum } from "@/Components/tasks/kanban/colum";
+import React from "react";
 
 const TaskList = () => {
   return (
-    <div>TaskList</div>
-  )
-}
+    <>
+      <KanbanBoardContainer>
+        <KanbanBoard>
+          <KanbanColum></KanbanColum>
+          <KanbanColum></KanbanColum>
+        </KanbanBoard>
+      </KanbanBoardContainer>
+    </>
+  );
+};
 
 export default TaskList;
