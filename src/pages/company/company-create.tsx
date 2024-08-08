c54b7928a565385e9fd45ff88a2b5aa7720c7e54
Éric  António <ericantoniodev@gmail.com>
2024-08-08 11:25:42 +0200

    Update company-create.tsx

--- src/pages/company/company-create.tsx
+++ src/pages/company/company-create.tsx
@@ -1,9 +1,11 @@
 import React, { act } from "react";
 import { CompanyList } from "./company-list";
-import { Button, Form, Input, Modal } from "antd";
+import { Button, Form, Input, Modal, Select } from "antd";
 import { useModalForm } from "@refinedev/antd";
 import { useGo } from "@refinedev/core";
+import { useSelect } from "@refinedev/antd";
 import { CREATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { USERS_SELECT_QUERY } from "@/graphql/queries";
 
 export const Create = () => {
   const go = useGo();
@@ -15,6 +17,14 @@ export const Create = () => {
     });
   };
 
+  const { slectProps, queryResults } = useSelect({
+    resource: "users",
+    optionLabel: "name",
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
+    },
+  });
+
   const { fromProps, modalProps } = useModalForm({
     action: "create",
     defaultVisible: true,
@@ -23,19 +33,28 @@ export const Create = () => {
     mutationMode: "pessimistic",
     onMutationSuccess: goToListPage,
     meta: {
-      gqlMutation: CREATE_COMPANY_MUTATION
+      gqlMutation: CREATE_COMPANY_MUTATION,
     },
   });
 
   return (
     <CompanyList>
-      <Modal
-      {...modalProps} mask={true} title='Create Company' width={512}>
-        <Form {...fromProps} layout='vertical'>
-            <Form.Item label='Company Name' name='name' rules={[{ required: true }]}>
-                <Input  placeholder="Please entre a Company Name"/>
-            </Form.Item>
-            
+      <Modal {...modalProps} mask={true} title="Create Company" width={512}>
+        <Form {...fromProps} layout="vertical">
+          <Form.Item
+            label="Company Name"
+            name="name"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="Please entre a Company Name" />
+          </Form.Item>
+          <Form.Item
+            label="Sales Owner"
+            name="SalesOwnerId"
+            rules={[{ required: true }]}
+          >
+            <Select placeholder="Sales Owner" {...slectProps} />
+          </Form.Item>
         </Form>
       </Modal>
     </CompanyList>
