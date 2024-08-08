eb1bb44076c10ab0d54a4aad972d3577fcee632f
Éric  António <ericantoniodev@gmail.com>
2024-08-08 17:54:53 +0200

    Update colum.tsx

--- src/Components/tasks/kanban/colum.tsx
+++ src/Components/tasks/kanban/colum.tsx
@@ -1,13 +1,19 @@
 import { Text } from "@/Components/text";
+import { PlusCircleOutlined } from "@ant-design/icons";
 import { useDroppable } from "@dnd-kit/core";
 
-import { Space } from "antd";
+import { Badge, Button, Space } from "antd";
+import { Children } from "react";
 
-
-export const KanbanColum = () => {
+export const KanbanColum = ({ children }: React.PropsWithChildren) => {
   const { isOver, setNodeRef, active } = useDroppable({
     id: "",
   });
+
+  const description = "Description";
+  const title = "Title";
+  const onAddClickHandler = () => {};
+  const count = 4;
   return (
     <div
       ref={setNodeRef}
@@ -16,9 +22,47 @@ export const KanbanColum = () => {
       <div style={{ padding: "12px" }}>
         <Space style={{ width: "100%", justifyContent: "space-between" }}>
           <Space>
-            <Text ellipsis={{ tooltip: "TITLE TO DO" }}> TITLE TODO</Text>
+            <Text
+              ellipsis={{ tooltip: title }}
+              strong
+              size="xs"
+              style={{
+                textTransform: "uppercase",
+                whiteSpace: "nowrap",
+              }}
+            >
+              {" "}
+              {title}
+            </Text>
+            {!!count && <Badge count={count} color="pink" />}
           </Space>
+          <Button
+            shape="circle"
+            icon={<PlusCircleOutlined />}
+            onClick={onAddClickHandler}
+          />
         </Space>
+        {description}
+        <div
+          style={{
+            flex: 1,
+            overflowY: active ? "unset" : "scroll",
+            border: "2px dashed transparent",
+            borderBlock: isOver ? "#000040" : "transparent",
+            borderRadius: "4px",
+          }}
+        >
+          <div
+            style={{
+              marginTop: "12px",
+              display: "flex",
+              flexDirection: "column",
+              gap: "8px",
+            }}
+          >
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
